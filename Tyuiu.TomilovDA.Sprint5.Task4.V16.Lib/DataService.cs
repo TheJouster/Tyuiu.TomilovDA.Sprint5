d484954a807cbd397322640d7a1c6a1057424b1d
TheJouster <tomilovdima999@gmail.com>
2024-11-24 17:40:44 +0500

    Task4

--- Tyuiu.TomilovDA.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.TomilovDA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.TomilovDA.Sprint5.Task4.V16.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
-            double x = double.Parse(strX);
+            double x = double.Parse(strX,System.Globalization.CultureInfo.InvariantCulture);
             double res = Math.Round(Math.Cos(x) + Math.Pow(x,2)-((2*x)/1.2),3);
             return res;
         }
